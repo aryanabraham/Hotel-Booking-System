05eb7317f35ba88698d73dcb8592b18b68cc34ab
Harshul Jain <harshulgangwal02@gmail.com>
2023-04-04 17:00:38 +0530

    Added room route and created an api to use that route

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 const db = require('./db');
+const roomsRoute = require('./routers/roomsRoute');
+
+app.use('/api/rooms', roomsRoute);
+
+
+
 const port = process.env.PORT || 5000;
 
 
