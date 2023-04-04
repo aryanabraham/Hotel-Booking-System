a2e73438666d5eb87417f43c1e9b0c00a527041e
Harshul Jain <harshulgangwal02@gmail.com>
2023-04-04 17:40:28 +0530

    added post method in room route

--- routers/roomsRoute.js
+++ routers/roomsRoute.js
@@ -11,5 +11,14 @@ router.get("/getallrooms", async(req,res)=>{
         return res.status(400).json({message : error});
     } 
 });
+router.post("/getroombyid", async(req,res)=>{
+    const roomid = req.body.id;
+    try{
+        const room = await Room.findOne({_id : roomid});
+        return res.json(room);
+    }catch(error){
+        return res.status(400).json({message : error});
+    } 
+});
 
 module.exports = router;
