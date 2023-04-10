dd0e19753fc54e1f322bb2016e3cac36dbc97706
Harshul Jain <harshulgangwal02@gmail.com>
2023-04-11 00:34:20 +0530

    Added get request for all bookings and added a post request for cancel booking

--- routers/bookingsRoute.js
+++ routers/bookingsRoute.js
@@ -70,6 +70,38 @@ router.post("/getbookingsbyuserid", async(req,res) => {
     }catch(error){
         return res.status(400).json({error});
     }
-})
+});
+
+router.post("/cancelbooking", async (req,res) =>{
+    const [bookingid, roomid] = req.body;
+
+    try{
+        const bookingitem = await Booking.findOne({_id : bookingid});
+
+        bookingitem.status = 'cancelled';
+        await bookingitem.save();
+        const room = await Room.findOne({_id : roomid});
+        const bookings = room.currentbookings;
+
+        const temp = bookings.filter(booking => booking.bookingid.toString() != bookingid);
+        room.currentbookings = temp;
+
+        await room.save();
+
+        res.send('Your booking cancelled successfully');
+    }catch(error){
+        return res.status(400).json({error});
+    }
+});
+
+router.get("/getallbookings", async (req,res)=>{
+    try{
+        const bookings = await Booking.find();
+        res.send(bookings);
+    }catch(error)
+    {
+        return res.status(400).json({error});
+    }
+});
 
 module.exports = router;
