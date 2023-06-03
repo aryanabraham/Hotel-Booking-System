03536f753ef1d91d37f4237123e8957b959e544a
Harshul Jain <harshulgangwal02@gmail.com>
2023-06-03 17:35:44 +0530

    Resolved more errors and added Location option

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -18,6 +18,7 @@ function Homescreen() {
 
   const [searchkey, setsearchkey] = useState("");
   const [type, settype] = useState("all");
+  const [location, setLocation] = useState("all");
 
   // useEffect(async () => {
   //   try {
@@ -54,90 +55,160 @@ function Homescreen() {
     fetchFun();
   }, []);
 
-  function filterByDate(dates) {
-    // setfromdate(moment(dates[0].format("DD-MM-YYYY")));
-    // settodate(moment(dates[1].format("DD-MM-YYYY")));
-    // console.log(fromdate);
-    // console.log(todate);
+  // function filterByDate(dates) {
+  //   // setfromdate(moment(dates[0].format("DD-MM-YYYY")));
+  //   // settodate(moment(dates[1].format("DD-MM-YYYY")));
+  //   // console.log(fromdate);
+  //   // console.log(todate);
 
+  //   const from = moment(dates[0].$d).format("DD-MM-YYYY");
+  //   const to = moment(dates[1].$d).format("DD-MM-YYYY");
+  //   setfromdate(from);
+  //   settodate(to);
+  //   // console.log(from);
+  //   // console.log(to);
+
+  //   var temprooms = [];
+  //   var availability = false;
+
+  //   for (const room of duplicaterooms) {
+  //     if (room.currentbookings.length > 0) {
+  //       for (const booking of room.currentbookings) {
+  //         if (
+  //           // !moment(
+  //           //   moment(dates[0])
+  //           //     .format("DD-MM-YYYY")
+  //           //     .isBetween(booking.fromdate, booking.todate)
+  //           // ) &&
+  //           // !moment(
+  //           //   moment(dates[1])
+  //           //     .format("DD-MM-YYYY")
+  //           //     .isBetween(booking.fromdate, booking.todate)
+  //           // )
+  //           !moment(moment(dates[0]).format("DD-MM-YYYY")).isSameOrAfter(
+  //             booking.fromdate
+  //           ) &&
+  //           !moment(moment(dates[0]).format("DD-MM-YYYY")).isSameOrBefore(
+  //             booking.todate
+  //           ) &&
+  //           !moment(moment(dates[1]).format("DD-MM-YYYY")).isSameOrAfter(
+  //             booking.fromdate
+  //           ) &&
+  //           !moment(moment(dates[1]).format("DD-MM-YYYY")).isSameOrBefore(
+  //             booking.todate
+  //           )
+  //         ) {
+  //           if (
+  //             moment(dates[0]).format("DD-MM-YYYY") !== booking.fromdate &&
+  //             moment(dates[0]).format("DD-MM-YYYY") !== booking.todate &&
+  //             moment(dates[1]).format("DD-MM-YYYY") !== booking.fromdate &&
+  //             moment(dates[1]).format("DD-MM-YYYY") !== booking.todate
+  //           ) {
+  //             availability = true;
+  //           }
+  //         }
+  //       }
+  //     }
+  //     if (availability == true || room.currentbookings.length == 0) {
+  //       temprooms.push(room);
+  //     }
+  //     setrooms(temprooms);
+  //   }
+  // }
+
+  function filterByDate(dates) {
     const from = moment(dates[0].$d).format("DD-MM-YYYY");
     const to = moment(dates[1].$d).format("DD-MM-YYYY");
     setfromdate(from);
     settodate(to);
-    // console.log(from);
-    // console.log(to);
-
-    var temprooms = [];
-    var availability = false;
-
-    for (const room of duplicaterooms) {
-      if (room.currentbookings.length > 0) {
-        for (const booking of room.currentbookings) {
-          if (
-            // !moment(
-            //   moment(dates[0])
-            //     .format("DD-MM-YYYY")
-            //     .isBetween(booking.fromdate, booking.todate)
-            // ) &&
-            // !moment(
-            //   moment(dates[1])
-            //     .format("DD-MM-YYYY")
-            //     .isBetween(booking.fromdate, booking.todate)
-            // )
-            !moment(moment(dates[0]).format("DD-MM-YYYY")).isSameOrAfter(
-              booking.fromdate
-            ) &&
-            !moment(moment(dates[0]).format("DD-MM-YYYY")).isSameOrBefore(
-              booking.todate
-            ) &&
-            !moment(moment(dates[1]).format("DD-MM-YYYY")).isSameOrAfter(
-              booking.fromdate
-            ) &&
-            !moment(moment(dates[1]).format("DD-MM-YYYY")).isSameOrBefore(
-              booking.todate
-            )
-          ) {
-            if (
-              moment(dates[0]).format("DD-MM-YYYY") !== booking.fromdate &&
-              moment(dates[0]).format("DD-MM-YYYY") !== booking.todate &&
-              moment(dates[1]).format("DD-MM-YYYY") !== booking.fromdate &&
-              moment(dates[1]).format("DD-MM-YYYY") !== booking.todate
-            ) {
-              availability = true;
-            }
-          }
-        }
-      }
-      if (availability == true || room.currentbookings.length == 0) {
-        temprooms.push(room);
-      }
-      setrooms(temprooms);
-    }
+  
+    const filteredRooms = filterRooms(type, location);
+    const availabilityRooms = filterAvailability(filteredRooms, from, to);
+    setrooms(availabilityRooms);
   }
+  
+  function filterAvailability(rooms, fromDate, toDate) {
+    const availableRooms = rooms.filter((room) => {
+      const isAvailable = room.currentbookings.every((booking) => {
+        const bookingFromDate = moment(booking.fromdate, "DD-MM-YYYY");
+        const bookingToDate = moment(booking.todate, "DD-MM-YYYY");
+        const fromDateBeforeBooking = moment(fromDate, "DD-MM-YYYY").isBefore(bookingFromDate);
+        const toDateAfterBooking = moment(toDate, "DD-MM-YYYY").isAfter(bookingToDate);
+        return fromDateBeforeBooking || toDateAfterBooking;
+      });
+      return isAvailable;
+    });
+    return availableRooms;
+  }
+  
+
+  // function filterBySearch() {
+  //   const temprooms = duplicaterooms.filter((room) =>
+  //     room.name.toLowerCase().includes(searchkey.toLowerCase())
+  //   );
+  //   setrooms(temprooms);
+  // }
 
   function filterBySearch() {
-    const temprooms = duplicaterooms.filter((room) =>
+    const filteredRooms = filterRooms(type, location);
+    const availabilityRooms = filterAvailability(filteredRooms, fromdate, todate);
+  
+    const temprooms = availabilityRooms.filter((room) =>
       room.name.toLowerCase().includes(searchkey.toLowerCase())
     );
     setrooms(temprooms);
   }
+  
 
+  // function filterByType(e) {
+  //   settype(e);
+  //   if (e !== "all") {
+  //     const temprooms = duplicaterooms.filter(
+  //       (room) => room.type.toLowerCase() === e.toLowerCase()
+  //     );
+  //     setrooms(temprooms);
+  //   } else {
+  //     setrooms(duplicaterooms);
+  //   }
+  // }
+
+  // function filterByLocation(location) {
+  //   setLocation(location);
+  //   if (location === "all") {
+  //     setrooms(duplicaterooms);
+  //   } else {
+  //     const temprooms = duplicaterooms.filter(
+  //       (room) => room.location.toLowerCase() === location.toLowerCase()
+  //     );
+  //     setrooms(temprooms);
+  //   }
+  // }
+  
   function filterByType(e) {
     settype(e);
-    if (e !== "all") {
-      const temprooms = duplicaterooms.filter(
-        (room) => room.type.toLowerCase() === e.toLowerCase()
-      );
-      setrooms(temprooms);
-    } else {
-      setrooms(duplicaterooms);
-    }
+    const filteredRooms = filterRooms(e, location);
+    setrooms(filteredRooms);
+  }
+  
+  function filterByLocation(location) {
+    setLocation(location);
+    const filteredRooms = filterRooms(type, location);
+    setrooms(filteredRooms);
+  }
+  
+  function filterRooms(selectedType, selectedLocation) {
+    const filteredRooms = duplicaterooms.filter((room) => {
+      const matchesType = selectedType === "all" || room.type.toLowerCase() === selectedType.toLowerCase();
+      const matchesLocation = selectedLocation === "all" || room.location.toLowerCase() === selectedLocation.toLowerCase();
+      return matchesType && matchesLocation;
+    });
+    return filteredRooms;
   }
 
   return (
     <div className="container">
       <div className="row mt-5 bs">
-        <div className="col-md-3">
+        <div className="col-md-3" style={{marginTop: '-10px'}}>
           <RangePicker format="DD-MM-YYYY" onChange={filterByDate} />
         </div>
         <div className="col-md-5">
@@ -163,6 +234,22 @@ function Homescreen() {
             <option value="all">All</option>
             <option value="deluxe">Deluxe</option>
             <option value="non-deluxe">Non-Deluxe</option>
+          </select>
+        </div>
+        <div className="col-md-3 p-3 mx-auto">
+          <select
+            className="form-control"
+            value={location}
+            onChange={(e) => {
+              filterByLocation(e.target.value);
+            }}
+          >
+          <option value="all">Location - All</option>
+            <option value="dehradun">Dehradun</option>
+            <option value="shimla">Shimla</option>
+            <option value="manali">Manali</option>
+            <option value="hyderabad">Hyderabad</option>
+            
           </select>
         </div>
       </div>
